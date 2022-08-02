d49e5d9a14938480981034b16a9c57a0a81ee07f
ji-minlee <lee.jimin907@gmail.com>
2022-08-02 13:27:01 +0900

    first commit

---
+++ ch02/Variable/VarEx1.java
@@ -0,0 +1,20 @@
+package ch02.Variable;
+
+public class VarEx1 {
+
+	public static void main(String[] args) {
+		
+		int year = 0;	//변수 year를 선언하고 0으로 초기화
+		int age = 14;	//변수 age를 선언하고 14로 초기화
+		
+		System.out.println(year);	//변수 year의 값을 출력
+		System.out.println(age);	//변수 age의 값을 출력
+		
+		year = age + 2000;	//변수 age에 저장된 값에 2000을 더한 후 그 결과를 변수 year에 저장
+		age = age + 1;	//변수 age에 저장된 값에 1을 더한 후 그 결과를 변수 age에 저장
+		
+		System.out.println(year);	//변수 year의 값을 출력
+		System.out.println(age);	//변수 age의 값을 출력
+	}
+
+}
---
+++ ch02/Variable/VarEx2.java
@@ -0,0 +1,21 @@
+package ch02.Variable;
+
+//두 변수의 값 교환하기	
+public class VarEx2 {
+
+	public static void main(String[] args) {
+		
+		int x = 10, y = 20;	//변수 x,y를 선언하고 각각 10, 20으로 초기화	
+							//변수 타입이 같은 경우 ,로 연결해 선언할 수 있다
+		int tmp = 0;	//변수 tmp를 선언하고 0으로 초기화
+						//x,y 두 변수의 값을 교환하기 위해서는 변수를 하나 더 선언해 임시 저장소로 사용
+		
+		System.out.println("x:" + x + " y:" + y);	//변수 x,y 출력
+			
+		tmp = x;	//변수 x에 저장된 값을 변수 tmp에 저장
+		x = y;	//변수 y에 저장된 값을 변수 x에 저장
+		y = tmp;	//변수 tmp에 저장된 값을 변수 y에 저장
+		
+		System.out.println("x:" + x + " y:" + y);	//변수 x,y 출력
+	}
+}
