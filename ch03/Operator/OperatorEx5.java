e83342ac43ebd8a3af1071f6e5b94cc9694c54df
ji-minlee <lee.jimin907@gmail.com>
2022-08-03 19:17:38 +0900

    사칙 연산자 기본

---
+++ ch03/Operator/OperatorEx5.java
@@ -0,0 +1,17 @@
+package ch03.Operator;
+
+public class OperatorEx5 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 4;
+		
+		System.out.printf("%d + %d = %d%n", a, b, a+b);
+		System.out.printf("%d - %d = %d%n", a, b, a-b);
+		System.out.printf("%d * %d = %d%n", a, b, a*b);
+		System.out.printf("%d / %d = %d%n", a, b, a/b);	//나누기 연산자의 두 피연산자가 모두 int인 경우, 연산결과도 int타입
+		System.out.printf("%d / %f = %f%n", a, (float)b, a/(float)b);
+
+	}
+
+}
