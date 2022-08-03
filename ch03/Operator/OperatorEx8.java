c11593a99764a85223975eea251b6efcea190808
ji-minlee <lee.jimin907@gmail.com>
2022-08-03 19:42:15 +0900

    같은 의미의 식, 연산 순서에 따라 다른 결과

---
+++ ch03/Operator/OperatorEx8.java
@@ -0,0 +1,16 @@
+package ch03.Operator;
+
+public class OperatorEx8 {
+
+	public static void main(String[] args) {
+		int a = 1000000;
+		
+		int result1 = a * a / a;
+		int result2 = a / a * a;
+		
+		System.out.printf("%d * %d / %d = %d%n", a, a, a, result1);		//먼저 곱하는 경우, int의 범위를 넘어서 오버 플로우 발생
+		System.out.printf("%d / %d * %d = %d%n", a, a, a, result2);		//먼저 나누는 경우, 정상적인 연산
+
+	}
+
+}
