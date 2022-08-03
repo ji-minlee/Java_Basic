9bc21206bf9f71c1336f31ec1c029c356f8151ae
ji-minlee <lee.jimin907@gmail.com>
2022-08-03 18:56:08 +0900

    메서드 호출에 사용된 증감 연산자

---
+++ ch03/Operator/OperatorEx3.java
@@ -0,0 +1,13 @@
+package ch03.Operator;
+
+public class OperatorEx3 {
+
+	public static void main(String[] args) {
+		int i = 5, j = 5;
+		System.out.println(i++);	// 출력 후 1 증가
+		System.out.println(++j);	// 1 증가 후 출력
+		System.out.println("i = " + i + ", j = " + j);	//각각 증가된 값으로 출력
+
+	}
+
+}
