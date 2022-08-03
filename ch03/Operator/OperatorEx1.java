592472826392b53db2591db6df0135bab1cb7110
ji-minlee <lee.jimin907@gmail.com>
2022-08-03 18:55:36 +0900

    증감 연산자

---
+++ ch03/Operator/OperatorEx1.java
@@ -0,0 +1,16 @@
+package ch03.Operator;
+
+public class OperatorEx1 {
+
+	public static void main(String[] args) {
+		int i = 5;
+		i++;	//i가 1 증가
+		System.out.println(i);
+		
+		i = 5;	//결과값 비교 위해 다시 5로 변경
+		++i;	//i가 1 증가
+		System.out.println(i);
+
+	}
+
+}
