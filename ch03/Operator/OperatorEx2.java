c226d30edfdb36cf868f7f1495d0bb3a2b59ed52
ji-minlee <lee.jimin907@gmail.com>
2022-08-03 18:55:55 +0900

    증감 연산자 전위형, 후위형

---
+++ ch03/Operator/OperatorEx2.java
@@ -0,0 +1,19 @@
+package ch03.Operator;
+
+public class OperatorEx2 {
+
+	public static void main(String[] args) {
+		int i = 5, j = 0;
+		
+		j = i++;	//후위형은 참조해 대입 후에 증가
+		System.out.println("j = i++; 실행 후, i = " + i + ", j = " + j);
+		
+		i = 5;
+		j = 0;
+		
+		j = ++i;	// 전위형은 증가 후에 참조해 대입
+		System.out.println("j = ++i; 실행 후, i = " + i + ", j = " + j);
+
+	}
+
+}
