bcd5b0c910855122c7db194ac077ad6a27d913cf
ji-minlee <lee.jimin907@gmail.com>
2022-08-04 19:58:03 +0900

    반올림

--- ch03/Operator/OperatorEx13.java
+++ ch03/Operator/OperatorEx13.java
@@ -8,6 +8,16 @@ public class OperatorEx13 {
 		//반올림을 위해 0.5를 더해줌
 		//소수점 넷째자리에서 반올림
 		System.out.println(shortPi);
+		
+		
+		
+		
+		//Math.round를 활용한 반올림
+		double pi2 = 3.141592;
+		double shortPi2 = Math.round(pi2 * 1000) / 1000.0;
+		
+		System.out.println(shortPi2);
+
 
 	}
 
