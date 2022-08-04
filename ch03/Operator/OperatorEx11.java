96ccae35aa3a18b53dcce0545cca2f9b85ce6c30
ji-minlee <lee.jimin907@gmail.com>
2022-08-04 19:51:19 +0900

    소문자를 대문자로 변환

--- ch03/Operator/OperatorEx11.java
+++ ch03/Operator/OperatorEx11.java
@@ -3,13 +3,10 @@ package ch03.Operator;
 public class OperatorEx11 {
 
 	public static void main(String[] args) {
-		
-		System.out.printf("10 == 10.0f %b%n", 10==10.0f);
-		
-		System.out.printf("0.1 == 0.1f %b%n", 0.1==0.1f);
-		//실수형은 근사값으로 저장되므로 오차가 발생할 수 있다 
-		//float타입과 double타입을 비교하려면 double타입을 float타입으로 형변환 후 비교해야 한다
-		
+		char lowerCase = 'a';
+		char upperCase = (char) (lowerCase - 32);	//소문자를 대문자로 변경하려면 A가 a보다 코드값이 32가 적으므로 a의 코드값에서 32를 빼면 됨
+		System.out.println(upperCase);
+
 	}
 
 }
