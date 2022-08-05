ba11ee470536952c0df4cbf9b19e6d6f13d4590a
ji-minlee <lee.jimin907@gmail.com>
2022-08-05 19:58:29 +0900

    do-while문

---
+++ ch04ControlStatement/FlowEx18.java
@@ -0,0 +1,29 @@
+package ch04ControlStatement;
+
+import java.util.Scanner;
+
+public class FlowEx18 {
+
+	public static void main(String[] args) {
+		int input = 0, answer = 0;
+		
+		answer = (int) (Math.random() * 100) + 1;	//1~100사이의 임의의 수 저장
+		Scanner sc = new Scanner(System.in);
+		
+		do {
+			System.out.print("1과 100사이의 정수를 입력하세요 > ");
+			input = sc.nextInt();
+			
+			if(input > answer) {
+				System.out.println("더 작은 수로 시도해 보세요");
+			} else if (input < answer) {
+				System.out.println("더 큰 수로 시도해 보세요");
+			}
+			
+		} while (input != answer);
+		
+		System.out.println("정답입니다.");
+
+	}
+
+}
