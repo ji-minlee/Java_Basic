a788a38dcd068a66a6da461d82cb96e696f2ac46
ji-minlee <lee.jimin907@gmail.com>
2022-08-05 19:01:00 +0900

    for문 기본형태

---
+++ ch04ControlStatement/FlowEx09.java
@@ -0,0 +1,14 @@
+package ch04ControlStatement;
+
+public class FlowEx09 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
