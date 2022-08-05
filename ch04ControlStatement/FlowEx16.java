9485c524d9d5414d08b924f33e9f7ee304962592
ji-minlee <lee.jimin907@gmail.com>
2022-08-05 19:47:45 +0900

    while문

--- ch04ControlStatement/FlowEx16.java
+++ ch04ControlStatement/FlowEx16.java
@@ -3,7 +3,11 @@ package ch04ControlStatement;
 public class FlowEx16 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int i = 5;
+		
+		while (i-- != 0) {
+			System.out.println(i + " - I can do it.");
+		}
 
 	}
 
